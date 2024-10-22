2dd12628ae3db3e1660f049a2c4219bb49fa9ed4
Victor Piguave <victorpiguave97@icloid.com>
2024-10-22 13:46:22 -0500

    melamin v1.0

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from .config import Config
+from app.config import Config
 from flask_login import LoginManager
 
 db = SQLAlchemy()
@@ -22,23 +22,23 @@ def create_app():
     from werkzeug.security import generate_password_hash
     app.register_blueprint(view, url_prefix="/")
 
-    with app.app_context():
-        if not Rol.query.filter_by(rol='admin' ).first():
-            rol_admin = Rol(rol = 'admin')
-            rol_user = Rol(rol='usuario')
-            db.session.add(rol_admin)
-            db.session.add(rol_user)
-            db.session.commit()
+    # with app.app_context():
+    #     if not Rol.query.filter_by(rol='admin' ).first():
+    #         rol_admin = Rol(rol = 'admin')
+    #         rol_user = Rol(rol='usuario')
+    #         db.session.add(rol_admin)
+    #         db.session.add(rol_user)
+    #         db.session.commit()
 
-        if not Usuario.query.filter_by(usuario='admin').first():
-            usuario = Usuario(
-                usuario='admin',
-                correo='admin@example.com',
-                clave= generate_password_hash('admin'),
-                rol_id = Rol.query.filter_by(rol="admin").first().id
-            )
-            db.session.add(usuario)
-            db.session.commit()
+    #     if not Usuario.query.filter_by(usuario='admin').first():
+    #         usuario = Usuario(
+    #             usuario='admin',
+    #             correo='admin@example.com',
+    #             clave= generate_password_hash('admin'),
+    #             rol_id = Rol.query.filter_by(rol="admin").first().id
+    #         )
+    #         db.session.add(usuario)
+    #         db.session.commit()
 
     return app
 
